70caaf66a83661a25f690b775395d28924300e20
Ch Ihtasham <cihtasham32@gmail.com>
2025-02-19 16:18:16 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,57 @@
 import { useState } from 'react'
-
 import UseCurrencyInfo from './hooks/UseCurrencyInfo'
-function App() {
-  const [count, setCount] = useState(0)
-  const currency = UseCurrencyInfo('usd')
-  return (
-    <div>
-      hello world
+import { InputBox } from './components'
 
+function App() {
+
+  const [amount, setAmount] = useState('')
+  const [from, setFrom] = useState('usd')
+  const [to, setTo] = useState('inr')
+  const [convertedAmount, setConvertedAmount] = useState('')
+  const currencyInfo = UseCurrencyInfo(from)
+  const options = currencyInfo ? Object.keys(currencyInfo) : [];
+  function convert() {
+    if (currencyInfo && currencyInfo[to]) {
+      setConvertedAmount(amount * currencyInfo[to]);
+    } else {
+      setConvertedAmount(0);
+    }
+  }
+  function swap() {
+    setFrom(preFrom => {
+      setTo(preFrom)
+      return to
+    })
+  }
+  return (
+
+    <div>
+      <h1>Currency Convertor</h1>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        convert()
+      }}>
+        <InputBox
+          label={'From'}
+          amount={amount}
+          currencyOptions={options}
+          onAmountChange={setAmount}
+          onCurrencyChange={setFrom}
+          selectCurrency={from}
+        />
+        <button onClick={swap}>swap</button>
+        <InputBox
+          label={'to'}
+          amount={convertedAmount}
+          currencyOptions={options}
+          onAmountChange={setConvertedAmount}
+          onCurrencyChange={setTo}
+          selectCurrency={to}
+        />
+        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+          Convert {from.toUpperCase()} to {to.toUpperCase()}
+        </button>
+      </form>
     </div>
   )
 }
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
-export default function InputBox() {
+export default function InputBox(
+    {
+        label,
+        amount,
+        onAmountChange,
+        selectCurrency,
+        onCurrencyChange,
+        currencyOptions = []
+    }
+) {
     return (
-        <div>
-
+        <div className='flex border border-black'>
+            <div className='flex flex-col'>
+                <label htmlFor="">{label}</label>
+                <input type="number"
+                    placeholder='Enter the amount'
+                    id=""
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(e.target.value)}
+                />
+            </div>
+            <div>
+                <div>Select Currency</div>
+                <select name="" id="" value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                >
+                    {currencyOptions.map((currency) => (
+                        <option key={currency} value={currency}>{currency}</option>
+                    ))}
+                </select>
+            </div>
         </div>
     )
 }
