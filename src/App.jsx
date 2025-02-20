38f6e25649b4743ecec760361ad286dd52c68893
Ch Ihtasham <cihtasham32@gmail.com>
2025-02-20 21:33:17 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ function App() {
       }}>
         <InputBox
           label={'From'}
+          
           amount={amount}
           currencyOptions={options}
           onAmountChange={setAmount}
